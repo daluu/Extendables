0aae5c8059a5364fabdd24a50152776c80d16f99
David Luu <mangaroo@gmail.com>
2015-05-08 18:37:57 -0700

    initial changes to unextend Extendables for testing and logging

---
+++ test/runner.unextended.framework.jsx
@@ -0,0 +1,34 @@
+﻿/* Modified barebones version of Extendables test framework
+ * containing only the core modules:
+ * logging, testing, templating, Jasmine
+ * as needed for testing scripts/code
+ * leaving out the extraneous object/array/string prototype "extensions", etc.
+ */
+
+//import Extendables minimum core libraries for testing
+#include "../core-packages/templating/lib/index.jsx";
+#include "../core-packages/testing/lib/index.jsx";
+#include "../core-packages/logging/lib/index.jsx";
+var tests = new TestRunner();
+var datetime = new Date();
+var timestamp = datetime.toDateString() + " " + datetime.getHours() + "-" + datetime.getMinutes();
+var log = new Log("WhateverYouNameThisTestSuiteResults " + timestamp + ".log",5); //under Extendables/log
+
+var specs = [];
+var tempSpecs = new File($.fileName).parent.getFiles("*.specs");
+for(var i = 0; i < tempSpecs.length; i++){
+	if(tempSpecs[i] instanceof File){ //or != Folder
+		specs.push(tempSpecs[i]);
+	}
+}
+
+for(var j = 0; j < specs.length; j++){
+	var specfile = specs[j];
+	try {
+		$.evalFile(specfile);
+	} catch (error) {
+		$.writeln(specfile + " is not a valid specifications file.\n" + error);
+	}
+}
+
+tests.to_html("WhateverYouNameThisTestSuiteResults " + timestamp + ".html");
